8f45f643ceeda0b6fed8526f40e1e3bb46dc7d32
anamohan <anandagarwaal@gmail.com>
2014-04-26 22:26:14 +0530

    Added solution for Counting Triangles

---
+++ TRICOUNT.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+	int t;
+    scanf("%d",&t);
+    while(t--) {
+        unsigned long long n;
+        scanf("%llu",&n);
+        unsigned long long ans;
+        ans = (n*(n+2)*(2*n+1)/8);
+        printf("%llu\n",ans);
+    }
+    return 0;
+}
